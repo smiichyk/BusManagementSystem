0aa0c5e7d713046627924cf95874e87500eeb803
Nikita Smiichyk <nikitasmiichyk@gmail.com>
2025-03-09 20:14:45 +0000

    0.1

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,3 +1,127 @@
 public class Bus {
 
+    // Fields
+    private int busNumber;
+    private String busName;
+    private String routeName;
+    private String departureTime;
+    private String arrivalTime;
+    private int capacity;
+    private String[] stops;
+
+    // Constructors
+    public Bus() {
+        this.busNumber=0;
+        this.busName="";
+        this.routeName="";
+        this.departureTime="";
+        this.arrivalTime="";
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber, String busName, String routeName, String departureTime, String arrivalTime, int capacity, String[] stops) {
+        this.busNumber=busNumber;
+        this.busName=busName;
+        this.routeName=routeName;
+        this.departureTime=departureTime;
+        this.arrivalTime=arrivalTime;
+        this.capacity=capacity;
+        this.stops=stops;
+    }
+    public Bus(int busNumber, String busName, String routeName, String departureTime, String arrivalTime, int capacity) {
+        this.busNumber=busNumber;
+        this.busName=busName;
+        this.routeName=routeName;
+        this.departureTime=departureTime;
+        this.arrivalTime=arrivalTime;
+        this.capacity=capacity;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber, String busName, String routeName, String departureTime, String arrivalTime) {
+        this.busNumber=busNumber;
+        this.busName=busName;
+        this.routeName=routeName;
+        this.departureTime=departureTime;
+        this.arrivalTime=arrivalTime;
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber, String routeName, String departureTime, String arrivalTime) {
+        this.busNumber=busNumber;
+        this.busName="";
+        this.routeName=routeName;
+        this.departureTime=departureTime;
+        this.arrivalTime=arrivalTime;
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber, String departureTime, String arrivalTime) {
+        this.busNumber=busNumber;
+        this.busName="";
+        this.routeName="";
+        this.departureTime=departureTime;
+        this.arrivalTime=arrivalTime;
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber, String departureTime) {
+        this.busNumber=busNumber;
+        this.busName="";
+        this.routeName="";
+        this.departureTime=departureTime;
+        this.arrivalTime="";
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber) {
+        this.busNumber=busNumber;
+        this.busName="";
+        this.routeName="";
+        this.departureTime="";
+        this.arrivalTime="";
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(int busNumber, String busName, int capacity) {
+        this.busNumber=busNumber;
+        this.busName=busName;
+        this.routeName="";
+        this.departureTime="";
+        this.arrivalTime="";
+        this.capacity=capacity;
+        this.stops=new String[0];
+    }
+    public Bus(String busName, String routeName) {
+        this.busNumber=0;
+        this.busName=busName;
+        this.routeName=routeName;
+        this.departureTime="";
+        this.arrivalTime="";
+        this.capacity=0;
+        this.stops=new String[0];
+    }
+    public Bus(String routeName, int busNumber, String[] stops) {
+        this.busNumber=busNumber;
+        this.busName="";
+        this.routeName=routeName;
+        this.departureTime="";
+        this.arrivalTime="";
+        this.capacity=0;
+        this.stops=stops;
+    }
+    public Bus(String[] stops) {
+        this.busNumber=0;
+        this.busName="";
+        this.routeName="";
+        this.departureTime="";
+        this.arrivalTime="";
+        this.capacity=0;
+        this.stops=stops;
+    }
+
+    // Getters
+
+
+    // Setters
+
 }
--- src/main/java/BusStation.java
+++ src/main/java/BusStation.java
@@ -1,2 +1,45 @@
 public class BusStation {
+
+    // Fields
+    private String stationName;
+    private String location;
+    private Bus[] buses;
+
+    // Constructors
+    public BusStation() {
+        this.stationName="";
+        this.location="";
+        this.buses=new Bus[0];
+    }
+    public BusStation(String stationName, String location, Bus[] buses) {
+        this.stationName=stationName;
+        this.location=location;
+        this.buses=buses;
+    }
+    public BusStation(String stationName, Bus[] buses) {
+        this.stationName=stationName;
+        this.location="";
+        this.buses=buses;
+    }
+    public BusStation(String stationName, String location) {
+        this.stationName=stationName;
+        this.location=location;
+        this.buses=new Bus[0];
+    }
+    public BusStation(String stationName) {
+        this.stationName=stationName;
+        this.location="";
+        this.buses=new Bus[0];
+    }
+    public BusStation(Bus[] buses) {
+        this.stationName="";
+        this.location="";
+        this.buses=buses;
+    }
+
+    // Getters
+
+
+    // Setters
+
 }
